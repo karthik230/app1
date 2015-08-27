b406bb5034b02404bf527386b102f0440915c466
karthik230 <shivakarthik.mallapu@gmail.com>
2015-08-27 01:36:17 -0700

    this is the new commit for $ value

--- test.rb
+++ test.rb
@@ -1,3 +1,6 @@
 hi this is the new file created in feature1 bracnh to see the changes
 now we are adding the new line
-now we are adding another line and performing statsh with this and lets see
+now we changed this line
+
+$a = 40;
+
