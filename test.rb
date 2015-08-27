35f47232e54172d6ee18106f11e5730cc8d6e662
karthik230 <shivakarthik.mallapu@gmail.com>
2015-08-27 01:24:46 -0700

    this is new commit right

--- test.rb
+++ test.rb
@@ -1,2 +1,3 @@
 hi this is the new file created in feature1 bracnh to see the changes
-now we are adding the new line 
+now we are adding the new line
+now we are adding another line and performing statsh with this and lets see
